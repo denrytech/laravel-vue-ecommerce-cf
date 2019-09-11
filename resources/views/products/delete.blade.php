0ccdc4849d8a20ed895585a319e0936d511493b8
Denry <denrytech@gmail.com>
2019-09-11 15:15:10 -0300

    authenticacion  control added

--- resources/views/products/delete.blade.php
+++ resources/views/products/delete.blade.php
@@ -1,5 +1,7 @@
+@auth
 {!! Form::open(['method' => 'DELETE', 'route' => ['productos.destroy', $product->id], 'onsubmit' => 'return confirm("Estás seguro de eliminar este producto?")']) !!}
 
 {!! Form::submit('Eliminar', ['class' => 'btn btn-danger']) !!}
 
 {!! Form::close() !!}
+@endauth

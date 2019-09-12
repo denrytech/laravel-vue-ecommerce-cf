b8e0307e4060b863af3840fd20784c20ce240d9e
Denry <denrytech@gmail.com>
2019-09-12 01:45:24 -0300

    vue animations (functional) component added

--- resources/js/components/products/ProductsComponent.vue
+++ resources/js/components/products/ProductsComponent.vue
@@ -1,10 +1,11 @@
 <template>
-	<div class="row justify-content-sm-center">
-		<product-card-component v-bind:product="product" v-for="product in products" v-bind:key="product.id"></product-card-component>
-	</div>
+	<material-transition-group tag="div" class="row justify-content-sm-center">
+		<product-card-component v-bind:product="product" v-for="(product,index) in products" v-bind:data-index="index" v-bind:key="product.id"></product-card-component>
+	</material-transition-group>
 </template>
 
 <script>
+import { setTimeout } from 'timers';
 	export default {
 		data() {
 			return {
@@ -21,7 +22,7 @@
 				axios.get(this.endpoint).then((response) => {
 					this.products = response.data.data;
 				});
-			}
+			},
 		}
 	}
 </script>
